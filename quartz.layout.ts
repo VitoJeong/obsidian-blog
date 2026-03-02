00356cb149eed649a2dafe36a3214ebe7d6b54c0
changha <chjeong@aift.kr>
2026-03-03 00:26:20 +0900

    layout.ts 수정

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,11 +1,14 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import RecentNotesForIndex from "./quartz/components/RecentNotesForIndex"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [Component.RecentNotesForIndex],
+  afterBody: [
+    RecentNotesForIndex,
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -21,11 +24,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    RecentNotesForIndex,
   ],
   left: [
     Component.PageTitle(),
-    Component.SocialIcons(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
@@ -35,7 +36,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-  ],
+  ]
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
@@ -43,7 +44,6 @@ export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
-    Component.SocialIcons(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
