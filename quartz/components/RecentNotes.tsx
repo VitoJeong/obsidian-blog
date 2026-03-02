95f87e546339b3c0ffde010184597d1deadbab31
changha <chjeong@aift.kr>
2026-03-03 00:42:12 +0900

    style: fix title-tags-date order and ensure horizontal lines appear correctly

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -45,7 +45,8 @@ export default ((userOpts?: Partial<Options>) => {
 
             return (
               <li class="recent-li">
-                <div class="recent-header">
+                <div class="section">
+                  {/* 1. 제목 */}
                   <div class="recent-title">
                     <h3>
                       <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
@@ -53,13 +54,8 @@ export default ((userOpts?: Partial<Options>) => {
                       </a>
                     </h3>
                   </div>
-                  {page.dates && (
-                    <div class="recent-date">
-                      <Date date={getDate(cfg, page)!} locale={cfg.locale} />
-                    </div>
-                  )}
-                </div>
-                <div class="section">
+
+                  {/* 2. 태그 */}
                   {opts.showTags && tags.length > 0 && (
                     <ul class="tags">
                       {tags.map((tag) => (
@@ -73,6 +69,13 @@ export default ((userOpts?: Partial<Options>) => {
                         </li>
                       ))}
                     </ul>
+                  )}
+
+                  {/* 3. 발행일 */}
+                  {page.dates && (
+                    <div class="recent-date">
+                      <Date date={getDate(cfg, page)!} locale={cfg.locale} />
+                    </div>
                   )}
                 </div>
               </li>
