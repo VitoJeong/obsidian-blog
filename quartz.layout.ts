d890f831a281d0642a7c96cef6834f18292a1182
changha <chjeong@aift.kr>
2026-03-02 23:50:30 +0900

    refactor: simplify RecentNotesForIndex by delegating to RecentNotes component

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [Component.RecentNotesForIndex()],
+  afterBody: [Component.RecentNotesForIndex],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
--- quartz/components/RecentNotesForIndex.tsx
+++ quartz/components/RecentNotesForIndex.tsx
@@ -1,85 +1,20 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import { FullSlug, resolveRelative } from "../util/path"
-import { byDateAndAlphabetical } from "./PageList"
-import { Date, getDate } from "./Date"
-import { i18n } from "../i18n"
-import { classNames } from "../util/lang"
-import style from "./styles/recentNotesForIndex.scss"
+import { QuartzComponent, QuartzComponentProps } from "./types"
+import RecentNotes from "./RecentNotes"
 
-interface Options {
-  title?: string
-  limit: number
-  showTags: boolean
-}
-
-const defaultOptions: Options = {
-  limit: 5,
-  showTags: true,
-}
-
-export default ((userOpts?: Partial<Options>) => {
-  const RecentNotesForIndex: QuartzComponent = ({
-    allFiles,
-    fileData,
-    displayClass,
-    cfg,
-  }: QuartzComponentProps) => {
-    const opts = { ...defaultOptions, ...userOpts }
-    if (fileData.slug !== "index") return null
-
-    const pages = allFiles
-      .filter((f) => f.slug !== "index")
-      .sort(byDateAndAlphabetical(cfg))
-      .slice(0, opts.limit)
-
-    return (
-      <div class={classNames(displayClass, "recent-notes")}>
-        <h3>{opts.title ?? i18n(cfg.locale).components.recentNotes.title}</h3>
-        <ul class="recent-ul">
-          {pages.map((page) => {
-            const title = page.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title
-            const tags = page.frontmatter?.tags ?? []
-
-            return (
-              <li class="recent-li">
-                <div class="recent-header">
-                  <div class="recent-title">
-                    <h3>
-                      <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
-                        {title}
-                      </a>
-                    </h3>
-                  </div>
-                  {page.dates && (
-                    <div class="recent-date">
-                      <Date date={getDate(cfg, page)!} locale={cfg.locale} />
-                    </div>
-                  )}
-                </div>
-                <div class="section">
-                  {opts.showTags && tags.length > 0 && (
-                    <ul class="tags">
-                      {tags.map((tag) => (
-                        <li>
-                          <a
-                            class="internal tag-link"
-                            href={resolveRelative(fileData.slug!, `tags/${tag}` as FullSlug)}
-                          >
-                            {tag}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    )
+const RecentNotesForIndex: QuartzComponent = (props: QuartzComponentProps) => {
+  if (props.fileData.slug === "index") {
+    return RecentNotes({
+      title: "Recent Posts",
+      limit: 10,
+      showTags: true,
+      linkToMore: false,
+      filter: (f) => f.slug !== "index",
+    })(props)
   }
+  return null
+}
 
-  RecentNotesForIndex.css = style
-  return RecentNotesForIndex
-}) satisfies QuartzComponentConstructor
+// Ensure styles from the inner RecentNotes component are included
+RecentNotesForIndex.css = RecentNotes({}).css
+
+export default RecentNotesForIndex
